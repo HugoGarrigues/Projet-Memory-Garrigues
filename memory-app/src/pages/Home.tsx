8e739e3125410d23b8212e7049b4eeb3ddc5e747
HugoGarrigues <113009447+HugoGarrigues@users.noreply.github.com>
2025-03-01 12:22:10 +0100

    feat(addDaisyUI): Config DaisyUI

--- memory-app/src/pages/Home.tsx
+++ memory-app/src/pages/Home.tsx
@@ -1,5 +1,11 @@
+import Card from "../components/Card";
+
 const Home = () => {
-    return <h1>Page Home</h1>;
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+    <Card title="Home"/>
+  </div>
+  );
   };
   
   export default Home;
