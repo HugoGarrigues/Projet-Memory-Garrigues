449f14b2d257d763e7453002ea0151c2f646de49
Hugo Garrigues <hugo.garrigues@ynov.com>
2025-03-07 20:03:08 +0100

    feat(addFooter): add an daisyUi footer

--- memory-app/src/App.tsx
+++ memory-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <AppRoutes />
+      <Footer />
     </BrowserRouter>
     
   );
