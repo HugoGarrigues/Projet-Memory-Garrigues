292cebdfa84174bb86c542df31f1b5aeafe092ce
Hugo Garrigues <hugo.garrigues@ynov.com>
2025-03-30 12:45:42 +0200

    feat(cardManagement): Create types folder and add models

---
+++ memory-app/src/types/card.ts
@@ -0,0 +1,9 @@
+export interface Card {
+  id: number;
+  front: string;
+  back: string;
+  level: number;
+  lastReviewed: Date;
+  nextReviewDate: Date;
+  themeId: number;
+}
---
+++ memory-app/src/types/theme.ts
@@ -0,0 +1,8 @@
+import { Card } from "./card";
+
+export interface Theme {
+    id: number;
+    name: string;
+    categoryId: number; 
+    cards: Card[];  
+  }
